074392256af0ce4a2ea8f1dd510bfdb41b822fc9
Montana Flynn <montana@montanaflynn.me>
2014-10-22 19:44:25 -0700

    Add code comments

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var onHeaders = require('on-headers')
+
 module.exports = function(){
+
+  // Return express middleware
 	return function(req, res, next) {
+
+    // Set request received header
 		res.setHeader('x-request-received', new Date().getTime())
-	  	onHeaders(res, function() {
+
+    // Define listener for when headers are sent
+	  onHeaders(res, function() {
+
+      // Set response sent header
 			this.setHeader('x-response-sent', new Date().getTime())
 	 	})
+
+    // Move on
 	 	next()
 	}
+
 }
